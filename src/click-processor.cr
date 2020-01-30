198e73db3cf29b700fcc98b171ae31e3db458d8b
Daniel Coulbourne <daniel@tighten.co>
2020-01-30 02:16:56 -0500

    Very basic concatenation.

--- src/click-processor.cr
+++ src/click-processor.cr
@@ -1,4 +1,5 @@
 require "option_parser"
+require "file_utils"
 
 module Click::Processor
   VERSION = "0.1.0"
@@ -19,8 +20,55 @@ module Click::Processor
 
       error = process.error.gets_to_end
       output = process.output.gets_to_end
+      processSuccess = process.wait.success?
 
-      puts process.wait.success? ? output : error
+      if !processSuccess
+        puts error
+        exit
+      end
+
+      directoryContents = output.chomp.split("\n")
+
+      directoryMp3Files = directoryContents.select! { |file| file.ends_with?(".mp3") }
+
+      if directoryMp3Files.empty?
+        puts "Sorry, there are no mp3 files in that directory."
+      end
+
+      FileUtils.cd(directory)
+      FileUtils.touch("click-concatenation-recipe.tmp")
+
+      concatenationRecipe = "# Concatenation Recipe for Click. Generated at #{Time.utc} \n" + directoryMp3Files.map! { |file| "file '#{FileUtils.pwd}/#{file}'" }.join("\n")
+      File.write("click-concatenation-recipe.tmp", concatenationRecipe)
+
+      concat = Process.new(
+        "ffmpeg",
+        [
+          "-f",
+          "concat",
+          "-safe",
+          "0",
+          "-i",
+          "click-concatenation-recipe.tmp",
+          "-c",
+          "copy",
+          "mixdown.mp3",
+        ],
+        output: Process::Redirect::Pipe,
+        error: Process::Redirect::Pipe
+      )
+      concatError = concat.error.gets_to_end
+      concatOutput = concat.output.gets_to_end
+      concatSuccess = concat.wait.success?
+
+      if !concatSuccess
+        puts concatError
+        exit
+      end
+
+      FileUtils.rm("click-concatenation-recipe.tmp")
+
+      puts "Success!"
     end
   end
 end
