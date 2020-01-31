cac7854746541cbde61efa24f8d19f66f6d2f9e8
Matthew Davis <matt@mattdavis.co.uk>
2020-01-31 16:08:15 +0000

    Add Mixer component

--- src/click-processor.cr
+++ src/click-processor.cr
@@ -2,10 +2,13 @@ require "dir"
 require "path"
 require "option_parser"
 require "./audio/concatenator"
+require "./audio/mixer"
 
 VERSION = "0.0.1"
 directory = ""
 output_filename = "mixdown-#{Time.utc}.mp3"
+concatenate = false
+mix = false
 
 # Handle the passed options
 OptionParser.parse do |parser|
@@ -14,10 +17,18 @@ OptionParser.parse do |parser|
   parser.on("-h", "--help", "Show Help") { puts parser }
   parser.on("-d DIRECTORY", "--directory=DIRECTORY", "Concatenate all audio files in a directory") { |dir| directory = Path[dir].expand(home: true).to_s }
   parser.on("-o FILENAME", "--output=FILENAME", "The output filename") { |filename| output_filename = Path[filename].expand(home: true).to_s }
+  parser.on("-c", "--concatenate", "Concatenate the files") { concatenate = true }
+  parser.on("-m", "--mix", "Mix the files") { mix = true }
 end
 
 # Ensure we have a directory and at least something for the output filename
 exit if directory.blank? || output_filename.blank?
+
+# Ensure we only have one mode set
+if (concatenate && mix) || (!concatenate && !mix)
+  puts "You must specify whether you want to concatenate or mix the files."
+  exit(1)
+end
 
 # Process...
 # 1. Get the MP3 files from the passed directory (removing the trailing slash if it has one)
@@ -29,8 +40,14 @@ if audioFiles.empty?
   exit(1)
 end
 
-# 3. Concatenate the MP3 files and output the concatenated audio
-Concatenator.new(audioFiles).run(output_filename)
+# 3. Concatenate or Mix the MP3 files and output the concatenated audio
+if concatenate
+  Concatenator.new(audioFiles).run(output_filename)
+end
+
+if mix
+  Mixer.new(audioFiles).run(output_filename)
+end
 
 # 4. Success message
 puts "Success"
